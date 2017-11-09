bfec720cae718afe7be0e8b8ef3a32042c246c7e
MFire30 <matheus_oliveira30@hotmail.com>
2017-11-08 22:02:33 -0200

    Creating initial JS dict

--- Bf2JS.py
+++ Bf2JS.py
@@ -3,8 +3,8 @@ import argparse
 # Adding args and description for pyhton3 file call on shell
 # Exemple: python3 bf2JS.py example1.bf -o output1.js
 parser = argparse.ArgumentParser(description='Process a brainfuck file to JS.')
-parser.add_argument("bfFile")
-parser.add_argument("-o")
+parser.add_argument('bfFile')
+parser.add_argument('-o')
 
 # Creates a namespace object for the args
 ARGUMENTS = parser.parse_args()
@@ -13,8 +13,30 @@ ARGUMENTS = parser.parse_args()
 INPUT_BF = ARGUMENTS.bfFile
 OUTPUT_JS = ARGUMENTS.o
 
+##
+ARRAY_NAME = 'bfArray'
+COUNTER_NAME = 'tapeReader'
+BFCHAR_TO_JS = {
+    # Increments the cell value
+    '+': '%s[%s]++\n' % (ARRAY_NAME, COUNTER_NAME),
+    # Decrements the cell value
+    '-': '%s[%s]--\n' % (ARRAY_NAME, COUNTER_NAME),
+    # Moves the pointer to the right
+    '>': '%s++\n' % (COUNTER_NAME),
+    # Moves the pointer to the left
+    '<': '%s--\n' % (COUNTER_NAME),
+    # Prints the cell value - ASCII
+    '.': 'String.fromCharCode(%s[%s])\n' % (ARRAY_NAME, COUNTER_NAME), 
+    # Gets a char and saves the value on cell
+    ',': '%s[%s] = prompt(\"Type a char\").charAt(0)' % (ARRAY_NAME, COUNTER_NAME),
+    # Starts the loop - If the cell is 0, goto matching ]
+    '[': 'while(%s) {\n' % (COUNTER_NAME),
+    # Finishes the loop - Back to matching [ if cell not null
+    ']': '}\n'
+}
+
 # This is the main function
 def main():
-    print "Initializing parser BF-2-JS"
+    print ('Initializing parser BF-2-JS')
 
 main()
